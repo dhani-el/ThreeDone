619fa71f4ef0874d90b4fcf0b021df55df26534b
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-09-21 00:43:54 +0100

    explore rapier

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
+import { useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+import { Suspense, useState } from "react";
 
 import Exploration from "./buildingExploration"
+import { Physics } from "@react-three/rapier";
 
 function App() {
+  
+  const [cameraPosition,setCameraPosition] = useState({x:0.5,y:0.7,z:0.5});
+  const [test,settest] = useState(1);
+
+  function handleKeyPress(key){
+    console.log(cameraPosition);
+    settest((init)=> init +=1)
+    console.log(key);
+    console.log(test);
+    switch(key){
+      case "w":  setCameraPosition((init)=>({...init,y:init.y + 0.2}));
+      break;
+      case "a":  setCameraPosition((init)=>({...init,x:init.x + 0.2}));
+      break;
+      case "s":  setCameraPosition((init)=>({...init,x:init.x - 0.2}));
+      break;
+      case "d":  setCameraPosition((init)=>({...init,y:init.y - 0.2}));
+      break;
+      case "q":  setCameraPosition((init)=>({...init,z:init.z + 0.2}));
+      break;
+      case "r":  setCameraPosition((init)=>({...init,z:init.z - 0.2}));
+      break;
+      default :  setCameraPosition((init)=>({...init,z:init.z - 0.2}));
+    }
+  }
+
+  // useEffect(()=>{
+  //   window.addEventListener("keypress",(e)=>{
+  //     handleKeyPress(e.key);
+  //   });
+
+  // },[])
+
+  useEffect(()=>{
+    console.log(test);
+    console.log(cameraPosition);
+    
+  },[test])
+
 
   return (
-    <div>
+    <div id="app" >
       <p>This is my react three fiber playground</p>
-      <Canvas>
-        <Exploration/>
+      <Canvas camera={{position:[cameraPosition.x,cameraPosition.y,cameraPosition.z]}} >
+        <OrbitControls/>
+        <Suspense fallback={<Html>Model is loading...</Html>}>
+          <Physics debug>
+            <Exploration cameraPositions={cameraPosition} />
+          </Physics>
+        </Suspense>
       </Canvas>
     </div>
   )
--- src/buildingExploration.jsx
+++ src/buildingExploration.jsx
@@ -1,15 +1,33 @@
-import { useLoader } from "@react-three/fiber";
+import { useEffect,useRef } from "react";
+import { useLoader,useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import  "./app.css";
+import { RigidBody } from "@react-three/rapier";
 
-export default function Exploration(){
-  const building = useLoader(GLTFLoader,"./coffee_stand.glb")
+export default function Exploration({cameraPositions}){
+  const building = useLoader(GLTFLoader,"./coffee_stand.glb");
+  const movingMeshRef = useRef();
+  // useFrame((state, delta) =>
+  // {
+
+  //     state.camera.position.x = cameraPositions.x;
+  //     state.camera.position.z = cameraPositions.z;
+  //     state.camera.position.y = cameraPositions.y;
+  
+  //     // ...
+  // })
+  useEffect(function(){
+    console.log("image has loaded");
+  },[])
+
   return  <>
-            <mesh>
+            {/* <mesh>
               <torusKnotGeometry/>
               <meshNormalMaterial/>
-            </mesh>
-
-          <primitive object={building.scene} />  
+            </mesh> */}
+            {/* <RigidBody ref = {movingMeshRef} > */}
+              <primitive position-y={-1} object={building.scene} />  
+            {/* </RigidBody> */}
+            <ambientLight intensity={2}/>
           </>
 }
