3e9c7450a1db356faf2f62c4c07ef2696972b6f6
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-10-07 07:33:33 +0100

    implement part labelling with drei html

--- src/util component/ModelToMesh.jsx
+++ src/util component/ModelToMesh.jsx
@@ -1,6 +1,8 @@
 // RenderMeshes.js
+import { Html,Text,Billboard } from '@react-three/drei'
 import { useMemo } from 'react'
 import * as THREE from 'three'
+
 
 export default function RenderMeshes({ gltf, clone = true }) {
   // Memoize to avoid re-rendering unnecessarily
@@ -23,9 +25,33 @@ export default function RenderMeshes({ gltf, clone = true }) {
 
   return (
     <>
-      {meshes.map((mesh, i) => (
-        <primitive key={i} object={mesh} scale= {0.1} />
-      ))}
+      {meshes.map((mesh, i) => {
+        const labelOffset = [0.2,0.2,0.2]
+        const box = new THREE.Box3().setFromObject(mesh);
+        const center = new THREE.Vector3();
+        box.getCenter(center);
+        const size = new THREE.Vector3();
+        box.getSize(size);
+        const labelPos = center.clone().add(new THREE.Vector3(...labelOffset).setY(size.y * 0.6))
+       return  <group>
+          <primitive key={i} object={mesh}  />
+          {/* <Text   font="https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxP.ttf"
+            fontSize={2}
+            position={[0, 0, 0]} 
+  color="black" anchorX="center" anchorY="middle"   characters="abcdefghijklmnopqrstuvwxyz0123456789!">
+          
+          </Text> */}
+          {/* <Billboard>
+            <Text font="https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Mu4mxP.ttf" fontSize={5} color="black">{mesh.name || `Mesh_${i}`}</Text>
+          </Billboard> */}
+
+          {/* <Html occlude position={labelPos} distanceFactor={8}>
+            <div style={{ color: 'white', fontSize: '14px' }}>{mesh.name || `Mesh_${i}`}</div>
+          </Html> */}
+        </group>
+        }
+      )
+      }
     </>
   )
 }
