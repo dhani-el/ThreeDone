80fe58179fb1f6d431fb68ce79a58dd3dfe946c7
omotayo daniel ukuhor <dani3lsavag3@gmail.com>
2025-10-09 12:53:06 +0100

    attach the position property of a movable physics based object to  the camera

--- src/practicePhysicsObject.jsx
+++ src/practicePhysicsObject.jsx
@@ -1,43 +1,63 @@
-import { RigidBody } from "@react-three/rapier";
+import { RigidBody, vec3 } from "@react-three/rapier";
 import { useEffect, useState, useRef } from "react";
+import { useThree } from "@react-three/fiber";
 
 export default function PracticeObject(){
   const direction = {forward:"forward",backward:"backward",left:"left",right:"right"};
   const [move,setMove] = useState({shouldMove:false,direction:direction.forward});
   const boxRef = useRef(null);
+  const {camera} = useThree();
+  const [cameraPosition, setCameraPosition] = useState(null)
 
-  function handleScroll(e){
 
-    // objectIsMoving = true;
 
-    if (e.deltaY > 0) {
-      if (boxRef.current) {
-        
-        boxRef.current.applyImpulse({ x: -10, y: 0, z: 0 }, true);
-        // boxRef.current.
-        console.log("moving forward");
-      }
-    }else{
-      boxRef.current.applyImpulse({ x: 10, y: 0, z: 0 }, true);
-      console.log("moving back");
-
-    }
-
-    // setTimeout(() => {
-    //   objectIsMoving = false;
-    // }, 500);
-
-  }
 
   useEffect(()=>{
-    // let objectIsMoving = false;
-    console.log(boxRef.current);
+    let objectIsMoving = false;
+    // console.log(boxRef.current);
 
+    function handleScroll(e){
+
+      if (objectIsMoving) {
+        return ;
+      }
+  
+      objectIsMoving = true;
+  
+      if (e.deltaY > 0) {
+        if (boxRef.current) {
+          boxRef.current.applyImpulse({ x: -10, y: 0, z: 0 }, true);
+          // boxRef.current.
+          setCameraPosition(()=>vec3(boxRef.current.translation()));
+          console.log(vec3(boxRef.current.translation()));
+          console.log("moving forward");
+        }
+      }else{
+        boxRef.current.applyImpulse({ x: 10, y: 0, z: 0 }, true);
+        setCameraPosition(()=>vec3(boxRef.current.translation()));
+        console.log(vec3(boxRef.current.translation()));
+        console.log("moving back");
+  
+      }
+  
+      setTimeout(() => {
+        objectIsMoving = false;
+      }, 250);
+  
+    }
 
     window.addEventListener("wheel",handleScroll);
 
     return ()=> {window.removeEventListener("wheel",handleScroll);}
   },[])
+
+  useEffect(()=>{
+    if (cameraPosition) {
+      camera.position.x = cameraPosition.x;
+      // camera.position.y = cameraPosition.y;
+      camera.position.z = cameraPosition.z;
+    }
+  },[cameraPosition])
 
   return (
             <RigidBody ref={boxRef} position={[0,0,0]} >
