08832cccc8a42329a2f92f7b65d0d3119701cc5e
daniel ukuhor <dani3lsavag3@gmail.com>
2026-05-28 20:44:16 +0100

    import all textures for the celestial bodies

--- src/FirstRer3FLesson/index.jsx
+++ src/FirstRer3FLesson/index.jsx
@@ -2,16 +2,22 @@ import {  useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { CelestialBody } from "../components/celestialBodies";
 import earth from "../assets/earth.jpeg";
+import mars from "../assets/2k_mars.jpg"
 
 export default function FirstLesson(){
-    const boxMeshRef = useRef(null);
+    const earthRef = useRef(null);
+    const marsRef = useRef(null);
 
     useFrame((state,delta)=>{
-        if(boxMeshRef.current){
-            boxMeshRef.current.rotation.y += delta * 2;
+        if(earthRef.current){
+            earthRef.current.rotation.y += delta * 3;
+            marsRef.current.rotation.y -= delta;
         }
     })
     return (
-        <CelestialBody ref={boxMeshRef} texture={earth} size={1} speed={0.5} materialType={"standard"}/>
+        <group>
+            <CelestialBody position={[0,0,0]} ref={earthRef} texture={earth} size={1} speed={0.5} materialType={"standard"}/>
+            <CelestialBody position={[0,0,3]} ref={marsRef} texture={mars} size={1} speed={0.5} materialType={"standard"}/>
+        </group>
     )
 }
--- src/components/celestialBodies.jsx
+++ src/components/celestialBodies.jsx
@@ -1,9 +1,9 @@
 import { useTexture } from "@react-three/drei"
 
-export function CelestialBody({ref,texture,size,speed,materialType}) {
+export function CelestialBody({ref,texture,size,speed,materialType,position}) {
     const textureMap = useTexture(texture);
     return (
-        <mesh ref={ref} >
+        <mesh ref={ref} position={position} >
             <sphereGeometry args={[size,32,32]} />
             {materialType === "basic" ? (
                 <meshBasicMaterial map={textureMap} />
